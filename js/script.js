282be5423585e1674702e2154f827e774ec527bf
emademan210-png <emademan210@gmail.com>
2026-07-19 13:04:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+ var degree = prompt("Enter degree");
+
+if (degree > 90)
+    alert("A");
+else if (degree > 80)
+    alert("B");
+else if (degree > 70)
+    alert("C");
+else
+    alert("F");    
